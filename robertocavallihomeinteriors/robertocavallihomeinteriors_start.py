89cb7b157eddb920699054b34b8aee5f6bdecb32
huangweiwei <vsonhuang@outlook.com>
2021-12-01 06:52:10 +0800

    robertocavallihomeinteriors 增加爬行逻辑

--- robertocavallihomeinteriors/robertocavallihomeinteriors_start.py
+++ robertocavallihomeinteriors/robertocavallihomeinteriors_start.py
@@ -1,3 +1,12 @@
+import datetime
+
+import pytz
 from scrapy import cmdline
 
-cmdline.execute(['scrapy', 'crawl', 'robertocavallihomeinteriors_detail'])
+# cmdline.execute(['scrapy', 'crawl', 'robertocavallihomeinteriors_detail'])
+
+today = datetime.datetime.now(pytz.timezone('PRC')).strftime("%Y-%m-%d_%H-%M-%S")
+
+cmdline.execute(['scrapy', 'crawl', 'robertocavallihomeinteriors_index',
+                 '-o', '{0}_{1}.json'.format('robertocavallihomeinteriors', today)
+                 ])
